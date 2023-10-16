07ea76602ef1efd714ee03976d04c98ef9aae26f
Osmeal <osmeal@floridauniversitaria.com>
2023-10-16 18:40:18 +0200

    Map obj y otras cosas

--- App.tsx
+++ App.tsx
@@ -16,19 +16,47 @@ import {
   View,
 } from 'react-native';
 
-import {Button}from 'react-native-paper'
+import { Button } from 'react-native-paper'
+
+const Saluda = (texto, indexo, nom) => {
+  return (<Text style={estil.sectionTitle}>{texto}{"\n"}Hola {nom} {indexo}</Text>);//un metodo solo puede devolver un componente
+};
+
+/* Las dos opciones valen
+const Saluda = (texto:string, nombre:string) => {
+  return <View>
+<Text style={estil.sectionTitle}>{texto}</Text>
+<Text style={estil.sectionTitle}>Hola {nombre}</Text>
+  </View>
+};
+*/
 
 const App = () => {
   //const isDarkMode = useColorScheme() === 'dark';
+  let esticLoguejat = true;
+  const alumnesDam = ["Borja", "Marcel", "Sergi", "Laia"];
 
+
+  //no hay que comparar con true, con nombre de variable se compara con true o usa !variable
   return (
-    <View >
-      <StatusBar/>
-      <Text style ={estil.sectionTitle}> Hola mundo </Text>
-      <Button icon='camera-plus'>Polsa ací ja!!</Button>
-    </View>
+    <Papel>
+      <StatusBar />
+      {
+        alumnesDam.map((unAlumne, index) => {
+          return(
+          Saluda("HBenvingut ", index, unAlumne)
+        )})
+
+      }
+    </Papel>
   );
 };
+
+//esticLoguejat && Saluda("Benvingut", "Oscar") es un if sense else resumit
+/*  esticLoguejat
+  ?Saluda("Benvingut", "Oscar")
+  :Saluda("Primer t'has de loguejar", "Anònim")
+  */
 
 const estil = StyleSheet.create({
 
